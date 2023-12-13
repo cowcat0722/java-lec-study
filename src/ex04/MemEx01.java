71ce89e58bdcd9f3f8948223fccf95247b2ed03a
Pakchan <marinas2575@naver.com>
2023-12-13 12:40:34 +0900

    ãStatic

---
+++ src/ex04/MemEx01.java
@@ -0,0 +1,13 @@
+package ex04;
+
+class Person1{
+    static int age = 20; // JVM을 실행하면 첫 번째로 static을 찾는다.
+    static char gender = '여';
+}
+
+public class MemEx01 {
+    public static void main(String[] args) {
+        System.out.println(Person1.age);
+        System.out.println(Person1.gender);
+    }
+}
