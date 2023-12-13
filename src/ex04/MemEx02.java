70bf19f1c00c4bebd22a40f1d7b1103d8a194893
Pakchan <marinas2575@naver.com>
2023-12-13 14:36:09 +0900

    add2

--- src/ex04/MemEx02.java
+++ src/ex04/MemEx02.java
@@ -11,7 +11,7 @@ public class MemEx02 {
         System.out.println("1년 지남");
         System.out.println("1년 지남");
 
-        Person2 p = new Person2(); // Person2 class안에 static을 넣지 않고 여기서부터 불러옴. 동적 할당
+        Person2 p = new Person2(); // Person2 class안에 static을 넣지 않고 여기서부터 불러옴. new 동적 할당
         System.out.println(p.age);
         System.out.println(p.gender);
 
