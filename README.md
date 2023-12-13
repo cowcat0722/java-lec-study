672ba8da0283f19a6ebe24cf28ba3f36e55f2029
Pakchan <marinas2575@naver.com>
2023-12-13 14:02:09 +0900

    README add

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 자바 스터디
 
 - 변수 (int, double, long, boolean, char)
+- 캐스팅(형변환)
+- 상수(변할수 없는 값, final)
+- 변수 작성법
+- Static
--- src/ex04/MemEx02.java
+++ src/ex04/MemEx02.java
@@ -1,9 +1,19 @@
 package ex04;
 
 class Person2 {
-
+    int age = 10;
+    char gender = '남';
 }
 
 public class MemEx02 {
+    public static void main(String[] args) {
+        System.out.println("1년 지남");
+        System.out.println("1년 지남");
+        System.out.println("1년 지남");
 
+        new Person2(); // Person2 class안에 static을 넣지 않고 여기서부터 불러옴. 동적 할당
+
+
+        System.out.println("메인 종료");
+    }
 }
