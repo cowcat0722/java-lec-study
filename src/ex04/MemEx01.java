671c65d89fa0d64448ca5e1f04f6c8fba82c6ba0
Pakchan <marinas2575@naver.com>
2023-12-13 14:14:16 +0900

    heap

--- src/ex04/MemEx01.java
+++ src/ex04/MemEx01.java
@@ -1,7 +1,7 @@
 package ex04;
 
 class Person1{
-    static int age = 20; // JVM을 실행하면 첫 번째로 static을 찾는다.
+    static int age = 20; // JVM을 실행하면 첫 번째로 static을 찾는다. 정적 메모리 할당
     static char gender = '여';
 }
 
--- src/ex04/MemEx02.java
+++ src/ex04/MemEx02.java
@@ -11,8 +11,9 @@ public class MemEx02 {
         System.out.println("1년 지남");
         System.out.println("1년 지남");
 
-        new Person2(); // Person2 class안에 static을 넣지 않고 여기서부터 불러옴. 동적 할당
-
+        Person2 p = new Person2(); // Person2 class안에 static을 넣지 않고 여기서부터 불러옴. 동적 할당
+        System.out.println(p.age);
+        System.out.println(p.gender);
 
         System.out.println("메인 종료");
     }
