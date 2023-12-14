422c9b2e1620bff0b4dcea9261279855a4f11290
Pakchan <marinas2575@naver.com>
2023-12-14 12:35:09 +0900

    1.1 + 0.1 == 1.2 false

---
+++ src/ex02/Programming/testest.java
@@ -0,0 +1,17 @@
+package ex02.Programming;
+
+public class testest {
+    public static void main(String[] args) {
+        double a = 0.1;
+        double b = 1.1;
+        double c = 1.2;
+
+        int i = 1,j = 2;
+        int m =3;
+        boolean d = (a+b == 1.2);
+        boolean k = (i+j==m);
+        System.out.println(d);
+        System.out.println(k);
+        System.out.println(a+b);
+    }
+}
