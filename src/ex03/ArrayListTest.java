e6429b05a71956846af5751f11301abe30d07c13
Pakchan <marinas2575@naver.com>
2023-12-15 11:18:30 +0900

    ArrayList

---
+++ src/ex03/ArrayListTest.java
@@ -0,0 +1,19 @@
+package ex03;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+       ArrayList<String> list = new ArrayList<>();
+       list.add("철수");
+       list.add("영희");
+       list.add("순신");
+       list.add("지영");
+
+       System.out.println(list.get(0));
+
+//       list.remove(0);
+
+//       System.out.println(list.get(0));
+    }
+}
