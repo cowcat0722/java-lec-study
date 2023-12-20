df34819069e9845b7eaea3a19779fad7990a2dbf
Pakchan <marinas2575@naver.com>
2023-12-20 11:08:22 +0900

    Class 기본문법

---
+++ src/ex04/BoxTest.java
@@ -0,0 +1,20 @@
+package ex04;
+class Box{
+    int width;
+    int length;
+    int heigth;
+    double getVolume(){
+        return (double) width*heigth*length;
+    }
+}
+
+public class BoxTest {
+    public static void main(String[] args) {
+        Box b = new Box();
+        b.width = 20;
+        b.length = 20;
+        b.heigth = 30;
+        System.out.println("상자의 가로, 세로, 높이는 "+b.width+", "+b.length+", "+b.heigth+"입니다.");
+        System.out.println("상자의 부피는 "+b.width*b.length*b.heigth+"입니다.");
+    }
+}
---
+++ src/ex04/Television.java
@@ -0,0 +1,23 @@
+package ex04;
+
+public class Television {
+
+    int chnnel; // 채널 번호
+    int volume; // 볼륨
+    boolean onOff; // 전원 상태
+
+    public static void main(String[] args) {
+        Television myTv = new Television();
+        myTv.chnnel = 7;
+        myTv.volume = 10;
+        myTv.onOff = true;
+
+        Television yourTv = new Television();
+        yourTv.chnnel = 9;
+        yourTv.volume = 12;
+        yourTv.onOff = true;
+        System.out.println("나의 텔레비젼의 채널은 "+myTv.chnnel+"이고 볼륨은 "+myTv.volume+"입니다.");
+        System.out.println("너의 텔레비젼의 채널은 "+yourTv.chnnel+"이고 볼륨은 "+yourTv.volume+"입니다.");
+
+    }
+}
