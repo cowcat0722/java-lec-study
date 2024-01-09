c26609e579777f96d176a62ed847cc99d00404b2
Pakchan <marinas2575@naver.com>
2024-01-09 15:46:52 +0900

    단방향 통신 복습

---
+++ src/ex17/oneway/Client.java
@@ -0,0 +1,29 @@
+package ex17.oneway;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1",10000);
+
+            Scanner sc = new Scanner(System.in);
+            String msg = sc.nextLine();
+
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream(),"UTF-8")
+            );
+
+            bw.write(msg+"\n");
+            bw.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
