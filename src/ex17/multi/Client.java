5693bc33e34216e7a0ec51b0ce1016a9cb8a81e4
Pakchan <marinas2575@naver.com>
2024-01-10 15:06:22 +0900

    전이중

--- src/ex17/multi/Client.java
+++ src/ex17/multi/Client.java
@@ -1,4 +1,55 @@
 package ex17.multi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+
+ //       try {
+            Socket socket = new Socket("192.168.0.93",30000); // 서버 연결
+
+            // send
+            Thread send = new Thread(() -> {
+                try {
+                    Scanner sc = new Scanner(System.in);
+                    PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
+
+                    while(true) {
+                        String sendMsg = sc.nextLine();
+                        pw.println(sendMsg);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            // receive
+            Thread receive = new Thread(() -> {
+                try {
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream(),"UTF-8")
+                    );
+
+                    while(true) {
+                        String receiveMsg = br.readLine();
+                        System.out.println("서버로 부터 받은 메시지 : " + receiveMsg);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } );
+
+            send.start();
+            receive.start();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+
+    }
 }
--- src/ex17/multi/Server.java
+++ src/ex17/multi/Server.java
@@ -1,4 +1,55 @@
 package ex17.multi;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server{
+    public static void main(String[] args) {
+
+        try {
+            // 클라이언트 연결
+            ServerSocket serverSocket = new ServerSocket(20000);
+            Socket socket = serverSocket.accept();
+
+            // receive
+            new Thread(() -> {
+                try {
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream(),"UTF-8")
+                    );
+                    while(true) {
+                        String receiveMsg = br.readLine();
+                        System.out.println("클라이언트로 부터 받은 메시지 : " + receiveMsg);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+
+            // send
+            new Thread(() -> {
+                try {
+                    PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
+                    Scanner sc = new Scanner(System.in);
+
+                    while(true) {
+                        String requestMsg = sc.nextLine();
+                        pw.println(requestMsg);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
