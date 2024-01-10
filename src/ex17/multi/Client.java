7b26139bd1059358d310313783eb56dd1d9f233a
Pakchan <marinas2575@naver.com>
2024-01-10 15:28:16 +0900

    전이중 통신 -1단계 클라이언트 -서버 전송 완료

--- src/ex17/multi/Client.java
+++ src/ex17/multi/Client.java
@@ -1,5 +1,33 @@
 package ex17.multi;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) {
+        try {
+            // 1. 소켓과 버퍼 만들기
+            Socket socket = new Socket("localhost",20000);
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
+            Scanner sc = new Scanner(System.in);
+
+            // 2. 메시지 전송 쓰레드
+            new Thread(() -> {
+                while(true){
+                    pw.println(sc.nextLine());
+                }
+            } ).start();
+
+
+            // 3. 메시지 읽기 스레드
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/ex17/multi/Server.java
+++ src/ex17/multi/Server.java
@@ -1,5 +1,38 @@
 package ex17.multi;
 
-public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 
+public class Server {
+    public static void main(String[] args) {
+        try {
+            // 1. 소켓과 버퍼 만들기
+            ServerSocket serverSocket = new ServerSocket(20000);
+            Socket socket = serverSocket.accept();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(),"UTF-8")
+            );
+            // 2. 메시지 받기 쓰레드
+            new Thread(() -> {
+                while(true) {
+                    try {
+                        String requestMsg = br.readLine();
+                        System.out.println("클라이언트로부터 받은 메시지 : " + requestMsg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 3. 메시지 전송 쓰레드
+
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
