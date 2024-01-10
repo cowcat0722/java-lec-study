d03c6c8c66da3786df7a3a9214248f3392d3fb6b
Pakchan <marinas2575@naver.com>
2024-01-10 11:49:48 +0900

    반이중 풀이

---
+++ src/ex17/half02/Server.java
@@ -0,0 +1,38 @@
+package ex17.half02;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(20000);
+            Socket socket = serverSocket.accept();
+            // 소켓 연결 완료됨
+
+            // 버퍼 만들기(reseive)
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(),"UTF-8")
+            );
+
+            String requestMsg = br.readLine();
+            System.out.println("클라이언트로부터 받은 메시지 : "+requestMsg);
+
+            // 버퍼 만들기(send)
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(),true, Charset.forName("UTF-8"));
+
+            if (requestMsg.equals("1")){
+                pw.println("영화");
+            }else if(requestMsg.equals("2")){
+                pw.println("드라마");
+            }else{
+                pw.println("프로토콜을 확인하세요 : 1은 영화, 2는 드라마");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/ex17/multi/Server.java
+++ src/ex17/multi/Server.java
@@ -1,45 +1,5 @@
 package ex17.multi;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-class Communication extends Thread{
-    @Override
-    public void run() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(10000);
-            Socket socket = serverSocket.accept();
-
-            System.out.println("클라이언트 연결됨");
-
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(),
-                            "UTF-8")
-            );
-
-            String msg = br.readLine();
-            System.out.println(msg);
-
-            BufferedWriter bw = new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream(),
-                            "UTF-8")
-            );
-
-            bw.write("상대방이 "+"\""+msg+"\""+" 메시지를 수신하였습니다"+"\n");
-            bw.flush();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-
-
-
 public class Server {
-    public static void main(String[] args) {
-        Communication communication = new Communication();
-        communication.start();
-    }
+
 }
